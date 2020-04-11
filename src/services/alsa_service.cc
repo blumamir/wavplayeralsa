57683c391ddda190090cfb2802959ac830707a07
amir <amirgiraffe@gmail.com>
2020-04-11 17:02:13 +0300

    init alsa in alsa service

--- src/services/alsa_service.cc
+++ src/services/alsa_service.cc
@@ -1,6 +1,7 @@
 #include "services/alsa_service.h"
 
 #include <sstream>
+#include <iostream>
 
 #include "alsa/asoundlib.h"
 #include "sndfile.hh"
@@ -18,12 +19,16 @@ namespace wavplayeralsa
         AlsaPlaybackService(
             std::shared_ptr<spdlog::logger> logger,
             const std::string &full_file_name, 
-            const std::string &file_id
+            const std::string &file_id,
+			const std::string &audio_device
         );
+
+		~AlsaPlaybackService();
 
     private:
 
-        void InitSndFile(const std::string &full_file_name);        
+        void InitSndFile(const std::string &full_file_name);  
+		void InitAlsa(const std::string &audio_device);      
 
 
     private:
@@ -43,6 +48,7 @@ namespace wavplayeralsa
 	    	SampleTypeFloat = 2
 	    };
 	    static const char *SampleTypeToString(SampleType sample_type);
+		bool GetFormatForAlsa(snd_pcm_format_t &out_format) const;
 
 		// from file
 	    unsigned int frame_rate_ = 44100;
@@ -55,19 +61,41 @@ namespace wavplayeralsa
 	    // calculated
 		unsigned int bytes_per_frame_ = 1;
 		snd_pcm_sframes_t frames_capacity_in_buffer_ = 0; // how many frames can be stored in a buffer with size TRANSFER_BUFFER_SIZE
+		
+		// alsa
+		snd_pcm_t *alsa_playback_handle_ = nullptr;
+
 
     };
 
     AlsaPlaybackService::AlsaPlaybackService(
             std::shared_ptr<spdlog::logger> logger,
             const std::string &full_file_name, 
-            const std::string &file_id
+            const std::string &file_id,
+			const std::string &audio_device
         ) :
             logger_(logger)
     {
         InitSndFile(full_file_name);
+		InitAlsa(audio_device);
     }
 
+	AlsaPlaybackService::~AlsaPlaybackService() {
+
+		if(alsa_playback_handle_ != nullptr) {
+			snd_pcm_close(alsa_playback_handle_);
+			alsa_playback_handle_ = nullptr;
+		}
+
+	}
+
+	/*
+	Read the file content from disk, extract relevant metadata from the
+	wav header, and save it to the relevant members of the class.
+	The function will also initialize the snd_file member, which allows to
+	read the wav file frames.
+	Will throw std::runtime_error in case of error.
+	 */
     void AlsaPlaybackService::InitSndFile(const std::string &full_file_name)
     {
 		snd_file_ = SndfileHandle(full_file_name);
@@ -164,6 +192,167 @@ namespace wavplayeralsa
 		throw std::runtime_error(err_desc.str());
 	}
 
+	/*
+	Init the alsa driver according to the params of the current wav file.
+	throw std::runtime_error in case of error
+	 */
+	void AlsaPlaybackService::InitAlsa(const std::string &audio_device) {
+
+		int err;
+		std::stringstream err_desc;
+
+		if( (err = snd_pcm_open(&alsa_playback_handle_, audio_device.c_str(), SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+			err_desc << "cannot open audio device " << audio_device << " (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		// set hw parameters
+
+		snd_pcm_hw_params_t *hw_params;
+
+		if( (err = snd_pcm_hw_params_malloc(&hw_params)) < 0 ) {
+			err_desc << "cannot allocate hardware parameter structure (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		if( (err = snd_pcm_hw_params_any(alsa_playback_handle_, hw_params)) < 0) {
+			err_desc << "cannot initialize hardware parameter structure (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		if( (err = snd_pcm_hw_params_set_access(alsa_playback_handle_, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+			err_desc << "cannot set access type (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		snd_pcm_format_t alsaFormat;
+		if(GetFormatForAlsa(alsaFormat) != true) {
+			err_desc << "the wav format is not supported by this player of alsa";
+			throw std::runtime_error(err_desc.str());
+		}
+		if( (err = snd_pcm_hw_params_set_format(alsa_playback_handle_, hw_params, alsaFormat)) < 0) {
+			err_desc << "cannot set sample format (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		if( (err = snd_pcm_hw_params_set_rate(alsa_playback_handle_, hw_params, frame_rate_, 0)) < 0) {
+			err_desc << "cannot set sample rate (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		if( (err = snd_pcm_hw_params_set_channels(alsa_playback_handle_, hw_params, num_of_channels_)) < 0) {
+			err_desc << "cannot set channel count (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		if( (err = snd_pcm_hw_params(alsa_playback_handle_, hw_params)) < 0) {
+			err_desc << "cannot set alsa hw parameters (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		snd_pcm_hw_params_free(hw_params);
+		hw_params = nullptr;
+
+
+		// set software parameters
+
+		snd_pcm_sw_params_t *sw_params;
+
+		if( (err = snd_pcm_sw_params_malloc(&sw_params)) < 0) {
+			err_desc << "cannot allocate software parameters structure (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		if( (err = snd_pcm_sw_params_current(alsa_playback_handle_, sw_params)) < 0) {
+			err_desc << "cannot initialize software parameters structure (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}
+
+		// how many frames should be in the buffer before alsa start to play it.
+		// we set to 0 -> means start playing immediately
+		if( (err = snd_pcm_sw_params_set_start_threshold(alsa_playback_handle_, sw_params, 0U)) < 0) {
+			err_desc << "cannot set start mode (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}	
+
+		if( (err = snd_pcm_sw_params(alsa_playback_handle_, sw_params)) < 0) {
+			err_desc << "cannot set software parameters (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}	
+
+		snd_pcm_sw_params_free(sw_params);
+		sw_params = nullptr;
+
+		if( (err = snd_pcm_prepare(alsa_playback_handle_)) < 0) {
+			err_desc << "cannot prepare audio interface for use (" << snd_strerror(err) << ")";
+			throw std::runtime_error(err_desc.str());
+		}	
+	}
+
+	bool AlsaPlaybackService::GetFormatForAlsa(snd_pcm_format_t &out_format) const {
+		switch(sample_type_) {
+
+			case SampleTypeSigned: {
+				if(is_endian_little_) {
+					switch(bytes_per_sample_) {
+						case 1: out_format = SND_PCM_FORMAT_S8; return true;
+						case 2: out_format = SND_PCM_FORMAT_S16_LE; return true;
+						case 3: out_format = SND_PCM_FORMAT_S24_LE; return true;
+						case 4: out_format = SND_PCM_FORMAT_S32_LE; return true;
+					}
+				}
+				else {
+					switch(bytes_per_sample_) {
+						case 1: out_format = SND_PCM_FORMAT_S8; return true;
+						case 2: out_format = SND_PCM_FORMAT_S16_BE; return true;
+						case 3: out_format = SND_PCM_FORMAT_S24_BE; return true;
+						case 4: out_format = SND_PCM_FORMAT_S32_BE; return true;
+					}
+				}
+			}
+			break;
+
+			case SampleTypeUnsigned: {
+				if(is_endian_little_) {
+					switch(bytes_per_sample_) {
+						case 1: out_format = SND_PCM_FORMAT_U8; return true;
+						case 2: out_format = SND_PCM_FORMAT_U16_LE; return true;
+						case 3: out_format = SND_PCM_FORMAT_U24_LE; return true;
+						case 4: out_format = SND_PCM_FORMAT_U32_LE; return true;
+					}
+				}
+				else {
+					switch(bytes_per_sample_) {
+						case 1: out_format = SND_PCM_FORMAT_U8; return true;
+						case 2: out_format = SND_PCM_FORMAT_U16_BE; return true;
+						case 3: out_format = SND_PCM_FORMAT_U24_BE; return true;
+						case 4: out_format = SND_PCM_FORMAT_U32_BE; return true;
+					}
+				}
+			}
+			break;
+
+			case SampleTypeFloat: {
+				if(is_endian_little_) {
+					switch(bytes_per_sample_) {
+						case 4: out_format = SND_PCM_FORMAT_FLOAT_LE; return true;
+						case 8: out_format = SND_PCM_FORMAT_FLOAT64_LE; return true;
+					}
+				}
+				else {
+					switch(bytes_per_sample_) {
+						case 4: out_format = SND_PCM_FORMAT_FLOAT_BE; return true;
+						case 8: out_format = SND_PCM_FORMAT_FLOAT64_BE; return true;
+					}
+				}			
+
+			}
+			break;
+
+		}
+		return false;
+	}
+
     void AlsaPlaybackServiceFactory::Initialize(
             std::shared_ptr<spdlog::logger> logger,
             const std::string &audio_device
@@ -179,9 +368,10 @@ namespace wavplayeralsa
         )
     {
         return new AlsaPlaybackService(
-            logger_->clone("alsa_playback_serrvice"), // TODO - use file name or id
+            logger_->clone("alsa_playback_service"), // TODO - use file name or id
             full_file_name,
-            file_id
+            file_id,
+			audio_device_
         );
     }
 
